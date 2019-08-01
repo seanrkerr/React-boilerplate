9b83bfcd9fd9a3afd5892ed4c30c705633d643b1
Sean Kerr <sean@seankerr.com>
2019-08-01 21:01:23 +1000

    add react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <h1>Hi From the boilerplate</h1>;
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
+  );
 };
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
 
 export default hot(module)(App);
