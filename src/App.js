391492ef24e070cfe7fb08e24239ea2345d3df7c
Sean Kerr <sean@seankerr.com>
2019-08-01 21:53:04 +1000

    add styled componments, rebass and some style

--- src/App.js
+++ src/App.js
@@ -1,40 +1,77 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { StyledSection } from "./style";
+import { createGlobalStyle } from "styled-components";
+import ReactIcon from "../src/assets/react-1.svg";
+import { Flex, Box } from "rebass";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #222;
+    color: white;
+  }
+  ul {
+    list-style-type: none;
+    display: flex;
+    li {
+      padding-right: 20px;
+      a {
+        color: white;
+      }
+    }
+  }
+  .container img {
+    display: block;
+    margin: 0 auto;
+  }
+  .container h2 {
+    text-align: center;
+  }
+`;
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-
+      <GlobalStyle />
+      <StyledSection>
+        <Flex flexDirection="cols">
+          <Box>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </Box>
+        </Flex>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-      </div>
+      </StyledSection>
     </Router>
   );
 };
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    // <div className="container">
+    //  c
+    //   <img src={ReactIcon} width="200px" />
+    // </div>
+    <div className="container">
+      <h2>React boilerplate</h2>
+      <img src={ReactIcon} width="200px" />
     </div>
   );
 }
 
 function About() {
   return (
-    <div>
+    <Flex flexDirection="rows" alignItems="center">
       <h2>About</h2>
-    </div>
+    </Flex>
   );
 }
 
