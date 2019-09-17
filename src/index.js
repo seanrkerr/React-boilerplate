275b505168b70f3e4b2cb602b1967f6cabb983da
Sean Kerr <sean@seankerr.com>
2019-09-17 22:22:09 +1000

    configure redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
 import App from "./App";
 
+const store = configureStore();
+
 function renderApp() {
-  render(<App />, root);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    root
+  );
 }
 
 renderApp();
