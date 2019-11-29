c704ba7c56908a6a33eb94b5dd6d2a9e66f6f8b2
Sean Kerr <sean@seankerr.com>
2019-11-29 20:09:46 +1100

    fix issue with the store not accepting more than two arguments

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/rootReducer";
 import thunk from "redux-thunk";
 
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+);
+
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-  );
+  return createStore(rootReducer, enhancer);
 }
